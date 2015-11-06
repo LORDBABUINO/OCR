3bd984800b0ee887892afe56888790319727dc77
WillDetlor <detl8600@mylaurier.ca>
2015-11-06 13:25:49 -0500

    Add apply filter

--- will.py
+++ will.py
@@ -1,7 +1,7 @@
 import numpy
 from PIL import Image
 
-myImage = Image.open("grayscale.jpg")
+myImage = Image.open("Lowpass_original.jpg").convert("L")
 
 def imgArr(myImage):
 
@@ -18,11 +18,57 @@ def imgArr(myImage):
 
 		# Add each value to the row
 		for j in range(width):
-			row.append(myImage.getpixel((j,i))[0])
+			row.append(myImage.getpixel((j,i)))
 
 		# Append the row to the main array
 		imgArr.append(row)
 
 	return imgArr
 
+# Input:
+#	myImage: the padded image to apply the filter to
+#	matrix: the matrix to use to multiply the image by
+# Output:
+#	Filtered image
+def applyFilter(myImage, kernel):
+
+	a = 0
+
+	offset = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,0], [0,1], [1,-1], [1,0], [1,1]]
+
+	for i in range(1, myImage.height-1):
+		for j in range(1, myImage.width-1):
+
+			for k in range(len(kernel)):
+				a += (myImage.getpixel((j+offset[k][0],i+offset[k][1])) * kernel[k])
+
+			myImage.putpixel((j,i), a)
+
+
+	return myImage
+
+def addPadding(im):
+	filtered = Image.new("L", (im.width + 2, im.height + 2), color = 0) #create new image that is 2 pixels wider and taller than the original
+
+	filtered.paste(im, (1, 1), None) #copy image into new image with padding of a 1 pixel border around
+
+	return filtered
+
+kernel = [-1,-1,-1,-1,8,-1,-1,-1,-1]
+
+print(myImage.size)
+
+myImage = addPadding(myImage)
+
+myImage.show()
+
 print(imgArr(myImage))
+
+print(myImage.size)
+
+myImage = applyFilter(myImage, kernel)
+
+print(imgArr(myImage))
+
+myImage.save("test.jpg")
+myImage.show()
