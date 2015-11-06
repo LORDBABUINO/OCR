0acb6dbfcb889d84b4c626b5540689ebb0ecd9c3
Jesper Leung <leun4090@mylaurier.ca>
2015-11-06 00:16:11 +0000

    added comments

--- jesper.py
+++ jesper.py
@@ -1,8 +1,13 @@
-from PIL import Image
-import time
-import sys
+# ------------------------------------------------------------------
+# CP467 Image Processing Demo
+# Friday November 6 2015
+# ------------------------------------------------------------------
 
+from PIL import Image        #our demo uses the Pillow library for importing images
+import time                  #used to add pauses in execution
+import sys                   #used to close program if user decides to quit
 
+#PRINTING MAIN MENU
 print("{0:-^50s}".format(''))
 print("{0:-^50s}".format('  Main Menu  '))
 print("{0:-^50s}".format(''))
@@ -12,24 +17,25 @@ print()
 print("{0:^50s}".format('1. Run demo'))
 print("{0:^50s}".format('2. Quit'))
 
+
 print()
-menuoption = input('Enter your choice here: ')
+menuoption = input('Enter your choice here: ')        #gets input from user
 print()
 
-if menuoption == "1":
+if menuoption == "1":            #user decided to run demo
     print("Demo started")
-    filename = input('Please enter the filename for the image you\'d like to use: ')
+    filename = input('Please enter the filename for the image you\'d like to use: ')    #get image filename from user
 
-    im = Image.open(filename)
+    im = Image.open(filename)    #import user-specified image
 
-    print("Image imported")
+    print("Image imported")      #status message
 
 // CODE FOR FILTERING GOES HERE
     
-    im.show()
+    im.show()                    #show image after filter
 
-elif menuoption == "2":
-    print("Quitting program..")
-    time.sleep(1.5)
+elif menuoption == "2":            #user decided to close the program
+    print("Quitting program..")    #display message to inform user that they're quitting the program
+    time.sleep(1.5)                #close after 1.5 seconds
     sys.exit
     
