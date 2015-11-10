480320193fac9606cbdaf4cdf4399422fc39cbe9
Jesper Leung <leun4090@mylaurier.ca>
2015-11-10 21:05:59 +0000

    convolution and filtering added

--- jesper.py
+++ jesper.py
@@ -1,41 +1,46 @@
-# ------------------------------------------------------------------
-# CP467 Image Processing Demo
-# Friday November 6 2015
-# ------------------------------------------------------------------
+from PIL import Image
+from PIL import ImageFilter
 
-from PIL import Image        #our demo uses the Pillow library for importing images
-import time                  #used to add pauses in execution
-import sys                   #used to close program if user decides to quit
+baseIMG = Image.open("dog.jpg").convert("L")
 
-#PRINTING MAIN MENU
-print("{0:-^50s}".format(''))
-print("{0:-^50s}".format('  Main Menu  '))
-print("{0:-^50s}".format(''))
-print("{0:^50s}".format('Welcome to the CP467 Final Project demo.'))
-print()
+def applyFilter(img, kernel):
+    i = 0        # initialize counter
+    j = 0
+    filtered = Image.new("L", (img.width - 2, img.height - 2))  #create new image 
 
-print("{0:^50s}".format('1. Run demo'))
-print("{0:^50s}".format('2. Quit'))
-
-
-print()
-menuoption = input('Enter your choice here: ')        #gets input from user
-print()
-
-if menuoption == "1":            #user decided to run demo
-    print("Demo started")
-    filename = input('Please enter the filename for the image you\'d like to use: ')    #get image filename from user
-
-    im = Image.open(filename)    #import user-specified image
-
-    print("Image imported")      #status message
-
-// CODE FOR FILTERING GOES HERE
+    imageArray = []        #used to pass pixels to convolution function
     
-    im.show()                    #show image after filter
+    for i in range(1, img.width - 1):    #for every pixel within padding
+        for j in range(1, img.height - 1):
+            del imageArray[:]        #clear imageArray each loop
+            imageArray.append(img.getpixel((i-1, j-1)))      #populate imageArray with top left pixel
+            imageArray.append(img.getpixel((i, j-1)))        #populate imageArray with top middle pixel
+            imageArray.append(img.getpixel((i+1, j-1)))      #populate imageArray with top right pixel
+            imageArray.append(img.getpixel((i-1, j)))        #populate imageArray with middle left pixel
+            imageArray.append(img.getpixel((i, j)))          #populate imageArray with middle pixel
+            imageArray.append(img.getpixel((i+1, j)))        #populate imageArray with middle right pixel
+            imageArray.append(img.getpixel((i-1, j+1)))      #populate imageArray with bottom left pixel
+            imageArray.append(img.getpixel((i, j+1)))        #populate imageArray with bottom middle pixel
+            imageArray.append(img.getpixel((i+1, j+1)))      #populate imageArray with bottom right pixel
+            filtered.putpixel((i- 1, j- 1), convolute(imageArray, kernel))    #write result of convolution to new image
+    return filtered
 
-elif menuoption == "2":            #user decided to close the program
-    print("Quitting program..")    #display message to inform user that they're quitting the program
-    time.sleep(1.5)                #close after 1.5 seconds
-    sys.exit
+def padImage(img):
+    padded = Image.new("L", (img.width + 2, img.height + 2), color = 0) #create new image
+
+    padded.paste(img, (1, 1), None)     #copy image into new image with padding
+
+    return padded
+
+def convolute(image, kernel):
+    temp = 0        #used to store current convolution sum
     
+    for i in range(0, len(image)):
+        temp = temp + (image[i]*kernel[i])    #increment temp by result of image pixel * kernel pixel
+
+    return temp
+    
+kernel = [-1, -1, -1, -1, 8, -1, -1, -1, -1]    #high pass filter
+padIMG = padImage(baseIMG)        #pad image with 0's
+filterIMG = applyFilter(padIMG, kernel)    #filter image with kernel
+filterIMG.save("output.jpg")        #save new image with filename "output.jpg"
