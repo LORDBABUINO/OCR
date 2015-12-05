391c0b50265aeb2d6d236be8bc30f1f4ed14f071
WillDetlor <detl8600@mylaurier.ca>
2015-12-05 18:39:02 -0500

    update BW function

--- production/whiteBlack.py
+++ production/whiteBlack.py
@@ -5,7 +5,7 @@ def convertBW(myImage):
 
 	for i in range(myImage.width):
 		for j in range(myImage.height):
-			if myImage.getpixel((i,j)) > 50:
+			if myImage.getpixel((i,j)) > 0:
 				myImage.putpixel((i,j), 255)
 			else:
 				myImage.putpixel((i,j), 0)
